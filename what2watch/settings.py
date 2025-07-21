b84b806eafdda0d210d28502d0c6896943d7931c
aryandhone555 <aryandhone555@gmail.com>
2025-07-21 14:50:47 +0530

    Updated CSVs and settings.py with recent changes

--- what2watch/settings.py
+++ what2watch/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'what2watch.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,  # keep connections open for 10 minutes
+        ssl_require=True
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
